bd3d358a1218ade1ae996e498233f6a7aa6eb033
fridlybot <146335388+fridlybot@users.noreply.github.com>
2023-09-28 11:11:19 +0100

    Create teghokservice.py

---
+++ teghokservice.py
@@ -0,0 +1,23 @@
+from flask import Flask, request
+import requests
+
+app = Flask(__name__)
+
+# Replace with your Google Apps Script web app URL
+GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbzRuc_TqUsLK7bmYj3QLvl8-MtpeGOCTV_dOoMLm2DzOK2wZAT7n0hRRA4-KMFv6wBWSw/exec"
+
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    data = request.json  # Assuming the incoming data is in JSON format
+    
+    # Send the data to Google Apps Script
+    response = requests.post(GOOGLE_SCRIPT_URL, json=data)
+    
+    # Handle the response from the Google Apps Script if needed
+    if response.status_code == 200:
+        return "Data sent to Google Apps Script successfully"
+    else:
+        return "Failed to send data to Google Apps Script"
+
+if __name__ == '__main__':
+    app.run(debug=True)
